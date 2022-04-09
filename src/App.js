cfbc916e411cc54ffae88f068264f883d38fb782
dokkalemon <dottori.roberto@gmail.com>
2022-04-09 16:07:21 +0200

    move data in externa file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React, {useState} from 'react'; 
 
 import { Container } from './components/UI/Container';
+import toDoList from './data'
 
 import './App.css';
 
 
 function App() {
-
-
-  //ToDo List
-  const toDoList = [
-    {
-      title: 'Porta a Spasso il cane',
-      doIt: false,
-      date: new Date('1/01/2022')
-    },
-    {
-      title: 'Lava la macchina',
-      doIt: false,
-      date: new Date('08/17/2021')
-    },
-    {
-      title: 'Vai a fare la spesa',
-      doIt: false,
-      date: new Date('07/14/2021')
-    },
-    {
-      title: 'Taglia l\'erba in Giardino',
-      doIt: false,
-      date: new Date('09/26/2021')
-    },
-  ]
 
   //Change Todo's State
   const [state, setState] = useState(toDoList);
