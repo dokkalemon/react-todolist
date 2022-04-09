eb1954ee15b69bd673b2a3f3d286a3bc960acb6b
dokkalemon <dottori.roberto@gmail.com>
2022-04-09 15:56:31 +0200

    fix error

--- src/components/NewTodo/TodoForm.js
+++ src/components/NewTodo/TodoForm.js
@@ -8,9 +8,9 @@ const TodoForm = (props) => {
         <div className="new-todo-form">
             <form onSubmit={props.saveTodo}>
                 <label htmlFor="text">Inserisci il Testo</label>
-                <input type="text" id="text" onChange={props.inputTextHandler}/>
+                <input type="text" id="text" onChange={props.inputTextHandler} required/>
                 <label htmlFor="data">Inserisci la data</label>
-                <input type="date" id="data" onChange={props.inputDateHandler}/>
+                <input type="date" id="data" onChange={props.inputDateHandler} required/>
                 <button type="submit">Salva</button>
             </form>
         </div>
